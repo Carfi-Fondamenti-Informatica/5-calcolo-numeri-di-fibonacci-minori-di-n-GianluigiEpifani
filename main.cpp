9021e070107b445676fa99855de314a11d7064af
GianluigiEpifani <101205771+GianluigiEpifani@users.noreply.github.com>
2022-04-05 18:39:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main (){
-    int a=0, b=1, g=0, n=0;
-    cout << "inserire un numero intero \n";
+
+int main() {
+    bool flag = true;
+    int n=0;
+    int fibonacci_p= 1, fibonacci=1;
+
     cin >> n;
-    if(n<=2){
-        for (int i=0;i<n;i++){
-            g=a+b;
-            b=a;
-            a=g;
-            cout << g << endl;
+
+    while(fibonacci <= n){
+        if(flag){
+            flag = false;
+            cout << fibonacci_p <<endl;
+        }else {
+            cout << fibonacci << endl;
+            int temp = fibonacci;
+            fibonacci += fibonacci_p;
+            fibonacci_p = temp;
         }
     }
-
     return 0;
 }
