aa84514644ce98adc18aa79bbe7a15e25c6bd70f
GianluigiEpifani <101205771+GianluigiEpifani@users.noreply.github.com>
2022-04-05 17:49:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main (){
+    int a=0, b=1, g=0, n=0;
+    cout << "inserire un numero intero \n";
+    cin >> n;
+    if(n<=2){
+        for (int i=0;i<n;i++){
+            g=a+b;
+            b=a;
+            a=g;
+            cout << g << endl;
+        }
+    }
+
+    return 0;
 }
